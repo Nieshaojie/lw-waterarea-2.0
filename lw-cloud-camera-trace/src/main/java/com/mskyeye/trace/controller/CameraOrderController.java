570df6edf3cb9314caeb3dd0e8efdaabf0bb2f74
nieshaojie <1191162363@qq.com>
2025-08-18 10:49:16 +0800

    滇池项目光电引导以及指示线监听调整

--- lw-cloud-camera-trace/src/main/java/com/mskyeye/trace/controller/CameraOrderController.java
+++ lw-cloud-camera-trace/src/main/java/com/mskyeye/trace/controller/CameraOrderController.java
@@ -455,7 +455,7 @@ public class CameraOrderController {
         if(tVal == null){
             if (yzCameraInfo.getManu().equals("gpl")) {
                 tVal = toDegrees(Math.atan2(height, dis)) + t_Val;
-                tVal = Math.max(-180, Math.min(180, tVal));
+                tVal = tVal < 0 ? 0 : tVal;
                 System.out.println("没有用曲线拟合方法计算T值——————原始t值："+toDegrees(Math.atan2(height, dis))+"————————补偿值："+t_Val+"————————最终t值："+tVal);
             }else{
                 tVal = -1 * toDegrees(Math.atan2(height, dis));
